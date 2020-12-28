943382ac1533abaafc85b02341d2a6d51845baf0
ojferro <ojferro@gmail.com>
2020-12-28 04:14:42 -0500

    Attepmt to ROSify

---
+++ Assets/Scripts/InterfaceControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Text;
+
+public class InterfaceControl : MonoBehaviour
+{
+    public Text timeDisplay;
+    public Text audioInText;
+    public static string data = null;
+
+    void Start(){}
+
+    void Update()
+    {
+        String time_hr = DateTime.Now.ToString("HH:mm");
+        timeDisplay.text = time_hr;
+        audioInText.text = RosSharp.RosBridgeClient.UserTextInSubscriber.text;
+    }
+}
---
+++ Assets/Scripts/UserTextInSubscriber.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class UserTextInSubscriber : UnitySubscriber<MessageTypes.Std.String>
+    {
+        // public Transform PublishedTransform;
+
+        public static string text;
+        public bool isMessageReceived;
+
+        protected override void Start()
+        {
+			base.Start();
+		}
+		
+        private void Update()
+        {
+            if (isMessageReceived)
+                ProcessMessage();
+        }
+
+        protected override void ReceiveMessage(MessageTypes.Std.String message)
+        {
+            text = message.data;
+            isMessageReceived = true;
+        }
+
+        private void ProcessMessage()
+        {}
+    }
+}
